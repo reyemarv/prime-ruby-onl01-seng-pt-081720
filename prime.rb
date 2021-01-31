7ab35dac71d26d2cc4a4fa3b3169427d35107c09
Marvin Reyes <reyemarv@gmail.com>
2021-01-31 05:15:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
-require "pry"
 
 def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
   else
     (2..num-1).to_a.all? do |possible_factor|
-      binding.pry
       num % possible_factor != 0
     end
   end
